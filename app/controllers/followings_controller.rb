31563afebb41b0fd7561a9e4d0d7c48db1f7ebe3
Eduardo Poleo <eduardopoleo@gmail.com>
2015-02-04 16:52:33 -0500

    Implemented create backnen for following model

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,6 +1,20 @@
 class FollowingsController < ApplicationController 
+  before_action :require_user
+
   def index
     @user = User.find(params[:user_id])
     @followings = @user.followings
+  end
+
+  def create
+    @following = Following.new(user_id: params[:user_id], subject_id: params[:subject_id]) 
+
+    if @following.save
+      flash[:notice] = "Your are now following #{User.find(params[:subject_id]).full_name}"
+    else
+      flash[:error] = "You already follow that person'"
+    end
+
+    redirect_to user_followings_path(current_user)
   end
 end
