f58378e7c00dacc5b9a4688486efedd5badb9003
Eduardo Poleo <eduardopoleo@gmail.com>
2015-02-06 14:18:59 -0500

    Fixed error with env variables

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(:default, Rails.env)
-
 module Myflix
   class Application < Rails::Application
     config.encoding = "utf-8"
