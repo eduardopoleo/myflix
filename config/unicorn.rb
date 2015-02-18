f3b851bf0bd6baa0abc48d6e00db70a7f6244327
Eduardo Poleo <eduardopoleo@gmail.com>
2015-02-18 17:41:52 -0500

    orgas unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,15 @@ before_fork do |server, worker|
   end
 
   defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  ActiveRecord::Base.connection.disconnect!
+
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
 end
 
 after_fork do |server, worker|
@@ -18,19 +26,5 @@ after_fork do |server, worker|
   end
 
   defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
-
-before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
-end
-
-worker_processes 3
-after_fork do |server, worker|
-  Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
-  end
-  Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
-  end
+  ActiveRecord::Base.establish_connection
 end
