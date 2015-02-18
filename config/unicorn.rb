d888b7a3b0e6080434e3df7142a19c710da8ea02
Eduardo Poleo <eduardopoleo@gmail.com>
2015-02-18 15:45:25 -0500

    Add code to unicorn file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,7 @@
+before_fork do |server, worker|
+   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+end
+
 worker_processes 3
 after_fork do |server, worker|
   Sidekiq.configure_client do |config|
