b6a546c064f3500cfe5797712759ef511ce71f7d
Eduardo Poleo <eduardopoleo@gmail.com>
2015-03-04 21:16:53 -0500

    Fix all broken specs

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -1,6 +1,4 @@
 class LargeCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  storage :fog
-
   process :resize_to_fill => [655, 375]
 end
--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -1,6 +1,4 @@
 class SmallCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  storage :fog
-
   process :resize_to_fill => [166, 236]
 end
